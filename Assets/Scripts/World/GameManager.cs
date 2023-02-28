d9c3ba9ccd0702d6065e21c81b1a32162ab478a6
Jose Manuel Marin <jmanuel2099@gmail.com>
2023-02-28 17:29:03 -0500

    Add Health and ammo in UI

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
-{
-    public static GameManager Instance {get; private set;}
+{   [Header("Commponentes UI")]
+    public Text ammoText;
+    public Text healthText;
+    public int health = 100;
     public int gunAmmo = 10;
+    public static GameManager Instance {get; private set;} // Singleton patern
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Update() 
+    {
+        ammoText.text = gunAmmo.ToString();
+
+        healthText.text = health.ToString();
+        
     }
 }
